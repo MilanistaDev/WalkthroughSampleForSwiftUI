9dd81e22090134240c5799861b163d95f477a9ed
Takuya Aso <milanista224@gmail.com>
2021-09-13 20:58:12 +0900

    Add UserDefaultsKey struct

---
+++ WalkthroughSampleForSwiftUI/Others/UserDefaultsKey.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKey.swift
+//  WalkthroughSampleForSwiftUI
+//
+//  Created by Takuya Aso on 2021/09/13.
+//
+
+import Foundation
+
+struct UserDefaultsKey {
+    static let isShownWalkthrough = "isShownWalkthrough"
+}
