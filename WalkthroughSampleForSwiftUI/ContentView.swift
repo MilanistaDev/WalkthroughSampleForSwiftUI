fb90ba551a8f64b7e3d7d0a85b100ad77487d526
Takuya Aso <milanista224@gmail.com>
2021-09-13 20:51:41 +0900

    Initial Commit

---
+++ WalkthroughSampleForSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalkthroughSampleForSwiftUI
+//
+//  Created by Takuya Aso on 2021/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WalkthroughSampleForSwiftUI/WalkthroughSampleForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkthroughSampleForSwiftUIApp.swift
+//  WalkthroughSampleForSwiftUI
+//
+//  Created by Takuya Aso on 2021/09/13.
+//
+
+import SwiftUI
+
+@main
+struct WalkthroughSampleForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
