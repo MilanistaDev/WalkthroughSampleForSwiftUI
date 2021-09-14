8d2d3898c6b11893d3566a70194b9ba30f08cbec
Takuya Aso <milanista224@gmail.com>
2021-09-15 06:14:13 +0900

    Add TabView with PageTabViewStyle.

--- WalkthroughSampleForSwiftUI/Views/Walkthrough/WalkthroughContentView.swift
+++ WalkthroughSampleForSwiftUI/Views/Walkthrough/WalkthroughContentView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct WalkthroughContentView: View {
+    // Gloomy
+    init() {
+        UIPageControl.appearance().currentPageIndicatorTintColor = .label
+        UIPageControl.appearance().pageIndicatorTintColor = .systemGray.withAlphaComponent(0.5)
+    }
+
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Text("Walkthrough Screen")
+                TabView {
+                    ForEach(0..<4) { index in
+                        Text("Walkthrough Screen" + index.description)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
                 FloatingButtonView()
                     .padding(.trailing, 28.0)
                     .padding(.bottom, geometry.safeAreaInsets.bottom + 16.0)
